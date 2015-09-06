082c00f805366fcf5127809d0937636bcab078e9
Alexander Lytkin <Serious-07@inbox.ru>
2015-09-06 13:33:52 +0600

    First commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+	public Vector2 newPos;
+	public Vector2 currentPos;
+	public int color;
+	public int angelGravity = 270;
+	public bool moveable = true;
+	public int x;
+	public int y;
+	public bool flag = false;
+
+	void Update(){
+		//this.gameObject.transform.rotation = new Quaternion ();
+		this.gameObject.transform.position = new Vector2 (10 + 30 * x, this.gameObject.transform.position.y);
+
+		/*float fY = Mathf.Round(this.gameObject.transform.position.y);
+		if ((fY % 10) == 0) {
+			flag = true;
+		}*/
+
+		/*for(int i = 0; i < StartGame.blocksPos.Length; i++){
+			if(this.gameObject.transform.position.x == StartGame.blocksPos[i]){
+				flag = true;
+				break;
+			}
+		}*/
+
+		if(flag){
+			Vector2 vec = new Vector2(this.gameObject.transform.position.x + 15, this.gameObject.transform.position.y - 0.1f);
+			//Collider2D[] hitColliders = Physics2D.OverlapPointAll(vec, 1);
+			Collider2D col = Physics2D.OverlapPoint(vec, 1);
+			//if (hitColliders.Length > 0) {
+			if (col != null) {
+				//this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = true;
+				this.gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+				float newY = Mathf.Round(this.gameObject.transform.position.y);
+				this.gameObject.transform.position = new Vector2 (this.gameObject.transform.position.x, newY);
+			//} else if(hitColliders.Length == 0){
+			} else {
+				//this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = false;
+				this.gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation;
+			}
+			flag = false;
+		}
+	}
+
+	void OnMouseDown(){
+
+		ArrayList blocks = getBlocks(this.gameObject);
+
+		if(blocks.Count >= 4){
+			for(int i = 0; i < blocks.Count; i++){
+				if((GameObject)blocks[i] != null){
+					Destroy((GameObject)blocks[i]);
+				}
+			}
+		}
+	}
+
+	public ArrayList getBlocks(GameObject block){
+		ArrayList objs = new ArrayList();
+
+		Block comp = block.GetComponent<Block>();
+		int blockX = comp.x;
+		int blockY = comp.y;
+
+		if(blockX == 0){
+			for(int i = 0; i < StartGame.xSize; i++){
+				GameObject obj = GameObject.Find(i + "-" + blockY);
+				if(obj != null){
+					if(obj.GetComponent<Block>().color == color){
+						objs.Add(obj);
+					} else {
+						break;
+					}
+				}
+			}
+		} else {
+			for(int i = blockX; i < StartGame.xSize; i++){
+				GameObject obj = GameObject.Find(i + "-" + blockY);
+				if(obj != null){
+					if(obj.GetComponent<Block>().color == color){
+						objs.Add(obj);
+					} else {
+						break;
+					}
+				}
+			}
+			
+			for(int i = blockX; i >= 0; i--){
+				GameObject obj = GameObject.Find(i + "-" + blockY);
+				if(obj != null){
+					if(obj.GetComponent<Block>().color == color){
+						objs.Add(obj);
+					} else {
+						break;
+					}
+				}
+			}
+		}
+		
+		if (blockY == 0) {
+			for (int i = 0; i < StartGame.ySize; i++) {
+				GameObject obj = GameObject.Find (blockX + "-" + i);
+				if(obj != null){
+					if(obj.GetComponent<Block>().color == color){
+						objs.Add(obj);
+					} else {
+						break;
+					}
+				}
+			}
+		} else {
+			for(int i = blockY; i < StartGame.ySize; i++){
+				GameObject obj = GameObject.Find(blockX + "-" + i);
+				if(obj != null){
+					if(obj.GetComponent<Block>().color == color){
+						objs.Add(obj);
+					} else {
+						break;
+					}
+				}
+			}
+			
+			for(int i = blockY; i >= 0; i--){
+				GameObject obj = GameObject.Find(blockX + "-" + i);
+				if(obj != null){
+					if(obj.GetComponent<Block>().color == color){
+						objs.Add(obj);
+					} else {
+						break;
+					}
+				}
+			}
+		}
+
+		/*for(int i = 0; i < objs.Count; i++){
+			if((GameObject)objs[i] != null){
+				ArrayList temp = getBlocks((GameObject)objs[i]);
+				for(int j = 0; j < temp.Count; j++){
+					objs.Add((GameObject)temp[j]);
+				}
+
+			}
+		}*/
+
+		return objs;
+	}
+}
---
+++ Assets/StartGame.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+	
+	public static int xSize = 10, ySize = 14;
+
+	public static int[] blocksPos = new int[ySize]; 	
+
+	int fl = 0;
+
+	void Start() {
+		for(int i = 0; i < xSize; i++){
+			for(int j = 0; j < ySize; j++){
+				GameObject obj = new GameObject();
+				obj.name = i + "-" + j;
+				//obj.tag = i + "-" + j;
+				obj.transform.position = new Vector2 (10 + 30 * i, 10 + 30 * j);
+				if(fl < 10){
+					blocksPos[fl] = 10 + 30 * i;
+					fl++;
+				}
+				obj.AddComponent<Block>();
+				obj.AddComponent<Rigidbody2D>();
+				obj.AddComponent<BoxCollider2D>();
+
+				BoxCollider2D coll = obj.GetComponent<BoxCollider2D>();
+				coll.offset = new Vector2(15f, 15f);
+				coll.size = new Vector2(30f, 30f);
+				coll.sharedMaterial = Resources.Load<PhysicsMaterial2D>("Block");
+
+				Rigidbody2D gravity = obj.GetComponent<Rigidbody2D>();
+				gravity.gravityScale = 5;
+				gravity.mass = 10;
+				gravity.angularDrag = 0;
+				//gravity.isKinematic = true;
+				gravity.collisionDetectionMode = CollisionDetectionMode2D.None;
+				gravity.interpolation = RigidbodyInterpolation2D.Interpolate;
+				gravity.sleepMode = RigidbodySleepMode2D.StartAsleep;
+				gravity.freezeRotation = true;
+				gravity.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+				Block block = obj.GetComponent<Block>();
+				block.currentPos = obj.transform.position;
+				block.color = Random.Range(1,6);
+				block.x = i;
+				block.y = j;
+				obj.AddComponent<SpriteRenderer>();
+				obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/" + obj.GetComponent<Block>().color);
+				//Instantiate(obj);
+			}
+		}
+	}
+}
